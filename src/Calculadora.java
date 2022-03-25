98a7f1e3b86471c0e458717267815658052bfe8d
Doiae9 <leonardogh75@gmail.com>
2022-03-25 11:14:31 -0600

    paso 2 getters y setters

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,4 +4,35 @@ public class Calculadora {
     private double primerValor;
     private double segundoValor;
 
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public boolean isEsCientifica() {
+        return EsCientifica;
+    }
+
+    public void setEsCientifica(boolean esCientifica) {
+        EsCientifica = esCientifica;
+    }
+
+    public double getPrimerValor() {
+        return primerValor;
+    }
+
+    public void setPrimerValor(double primerValor) {
+        this.primerValor = primerValor;
+    }
+
+    public double getSegundoValor() {
+        return segundoValor;
+    }
+
+    public void setSegundoValor(double segundoValor) {
+        this.segundoValor = segundoValor;
+    }
 }
